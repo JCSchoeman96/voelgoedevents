07144cf930073448ed6a1478b5f3c015e88f348b
JCSchoeman96 <jcschoeman123@gmail.com>
2025-12-02 22:31:18 +0200

    Backfill organization on financial ledgers migration

--- priv/repo/migrations/20251203130000_add_organization_to_financial_ledgers.exs
+++ priv/repo/migrations/20251203130000_add_organization_to_financial_ledgers.exs
@@ -1,7 +1,7 @@
 defmodule Voelgoedevents.Repo.Migrations.AddOrganizationToFinancialLedgers do
   use Ecto.Migration
 
-  def change do
+  def up do
     alter table(:financial_ledgers) do
       add :organization_id,
           references(:organizations,
@@ -9,9 +9,42 @@ defmodule Voelgoedevents.Repo.Migrations.AddOrganizationToFinancialLedgers do
             type: :uuid,
             on_delete: :nothing
           ),
-          null: false
+          null: true
     end
 
+    execute(
+      """
+      UPDATE financial_ledgers AS fl
+      SET organization_id = org.id
+      FROM (
+        SELECT id
+        FROM organizations
+        ORDER BY inserted_at ASC
+        LIMIT 1
+      ) AS org
+      WHERE fl.organization_id IS NULL
+      """
+    )
+
+    execute(
+      """
+      ALTER TABLE financial_ledgers
+      ALTER COLUMN organization_id SET NOT NULL
+      """,
+      """
+      ALTER TABLE financial_ledgers
+      ALTER COLUMN organization_id DROP NOT NULL
+      """
+    )
+
     create index(:financial_ledgers, [:organization_id])
+  end
+
+  def down do
+    drop index(:financial_ledgers, [:organization_id])
+
+    alter table(:financial_ledgers) do
+      remove :organization_id
+    end
   end
 end

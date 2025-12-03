5e8cdb1c6747035cc11f03dac12e501feeeec776
JCSchoeman96 <jcschoeman123@gmail.com>
2025-12-03 08:49:09 +0200

    Scope roles to organizations

--- priv/repo/migrations/20251205090000_create_roles.exs
+++ priv/repo/migrations/20251205090000_create_roles.exs
@@ -4,6 +4,14 @@ defmodule Voelgoedevents.Repo.Migrations.CreateRoles do
   def up do
     create table(:roles, primary_key: false) do
       add :id, :uuid, null: false, default: fragment("gen_random_uuid()"), primary_key: true
+      add :organization_id,
+          references(:organizations,
+            column: :id,
+            name: "roles_organization_id_fkey",
+            type: :uuid,
+            prefix: "public"
+          ),
+          null: false
       add :name, :text, null: false
       add :display_name, :text, null: false
       add :permissions, {:array, :text}, null: false, default: []
@@ -17,11 +25,14 @@ defmodule Voelgoedevents.Repo.Migrations.CreateRoles do
         default: fragment("(now() AT TIME ZONE 'utc')")
     end
 
-    create unique_index(:roles, [:name])
+    create index(:roles, [:organization_id])
+    create unique_index(:roles, [:organization_id, :name])
   end
 
   def down do
-    drop_if_exists index(:roles, [:name])
+    drop_if_exists index(:roles, [:organization_id, :name])
+    drop_if_exists index(:roles, [:organization_id])
+    drop constraint(:roles, "roles_organization_id_fkey")
     drop table(:roles)
   end
 end
